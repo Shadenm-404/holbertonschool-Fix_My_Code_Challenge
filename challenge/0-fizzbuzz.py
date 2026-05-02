415f53270d600cc7b0db40e2d2f4e7b1263096b2
Shaden Alalwani <viuxwl7@gmail.com>
2026-05-02 12:28:19 +0300

    Refactor FizzBuzz logic into functions
    Refactor FizzBuzz implementation to separate logic into build_value function and main function. Update argument handling and result printing.

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -1,39 +1,33 @@
 #!/usr/bin/python3
-""" FizzBuzz
-"""
+"""FizzBuzz implementation"""
+
 import sys
 
 
-def fizzbuzz(n):
-    """
-    FizzBuzz function prints numbers from 1 to n separated by a space.
-
-    - For multiples of three print "Fizz" instead of the number and for
-      multiples of five print "Buzz".
-    - For numbers which are multiples of both three and five print "FizzBuzz".
-    """
-    if n < 1:
-        return
-
-    tmp_result = []
-    for i in range(1, n + 1):
-        if (i % 3) == 0 and (i % 5) == 0:
-            tmp_result.append("FizzBuzz")
-        elif (i % 3) == 0:
-            tmp_result.append("Fizz")
-        elif (i % 5) == 0:
-            tmp_result.append("Buzz")
-        else:
-            tmp_result.append(str(i))
-    print(" ".join(tmp_result))
+def build_value(num):
+    if num % 15 == 0:
+        return "FizzBuzz"
+    if num % 3 == 0:
+        return "Fizz"
+    if num % 5 == 0:
+        return "Buzz"
+    return str(num)
 
 
-if __name__ == '__main__':
-    if len(sys.argv) <= 1:
+def main():
+    if len(sys.argv) != 2:
         print("Missing number")
         print("Usage: ./0-fizzbuzz.py <number>")
         print("Example: ./0-fizzbuzz.py 89")
         sys.exit(1)
 
-    number = int(sys.argv[1])
-    fizzbuzz(number)
+    limit = int(sys.argv[1])
+    if limit < 1:
+        return
+
+    result = [build_value(i) for i in range(1, limit + 1)]
+    print(" ".join(result))
+
+
+if __name__ == "__main__":
+    main()
