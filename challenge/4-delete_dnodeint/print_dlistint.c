4c79fa6cb7284c408560c6714cace9d8f86350be
Shaden Alalwani <viuxwl7@gmail.com>
2026-05-02 13:16:45 +0300

    Fix print_dlistint properly

--- challenge/4-delete_dnodeint/print_dlistint.c
+++ challenge/4-delete_dnodeint/print_dlistint.c
@@ -1,11 +1,12 @@
 #include "lists.h"
+
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
+
 	while (h)
 	{
-		printf("%d
-", h->n);
+		printf("%d\n", h->n);
 		h = h->next;
 		count++;
 	}
