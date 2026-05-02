4757c2a94d06d4e6d3c62569fbe49a329d2b125e
Shaden Alalwani <viuxwl7@gmail.com>
2026-05-02 12:01:04 +0300

    Fix final trailing space

--- 0-fizzbuzz.py
+++ 0-fizzbuzz.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 import sys
 
-res = []
-for i in range(1, int(sys.argv[1]) + 1):
+n = int(sys.argv[1])
+for i in range(1, n + 1):
     if i % 3 == 0 and i % 5 == 0:
-        res.append("FizzBuzz")
+        word = "FizzBuzz"
     elif i % 3 == 0:
-        res.append("Fizz")
+        word = "Fizz"
     elif i % 5 == 0:
-        res.append("Buzz")
+        word = "Buzz"
     else:
-        res.append(str(i))
-print(" ".join(res))
+        word = str(i)
+    if i == n:
+        print(word, end="")
+    else:
+        print(word, end=" ")
+print()
