5e90df00711a720d6a7e3cc86a68f7827b2fe047
Shaden Alalwani <viuxwl7@gmail.com>
2026-05-02 12:45:05 +0300

    Add delete dnodeint fix

---
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *node;
+	unsigned int i;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+
+	node = *head;
+	for (i = 0; node != NULL && i < index; i++)
+		node = node->next;
+
+	if (node == NULL)
+		return (-1);
+
+	if (node->prev != NULL)
+		node->prev->next = node->next;
+	else
+		*head = node->next;
+
+	if (node->next != NULL)
+		node->next->prev = node->prev;
+
+	free(node);
+	return (1);
+}
