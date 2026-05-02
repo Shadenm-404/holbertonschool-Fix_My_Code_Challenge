d5c707544cba2467b0ec496dd87e4949adf6f263
Shaden Alalwani <viuxwl7@gmail.com>
2026-05-02 13:12:03 +0300

    Fix double linked list files correctly

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -1,29 +1,15 @@
 #include "lists.h"
-
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *node;
-	unsigned int i;
-
-	if (head == NULL || *head == NULL)
-		return (-1);
-
+	unsigned int i = 0;
+	if (!head || !*head) return -1;
 	node = *head;
-	for (i = 0; node != NULL && i < index; i++)
-		node = node->next;
-
-	if (node == NULL)
-		return (-1);
-
-	if (node->prev != NULL)
-		node->prev->next = node->next;
-	else
-		*head = node->next;
-
-	if (node->next != NULL)
-		node->next->prev = node->prev;
-
+	while (node && i < index) { node = node->next; i++; }
+	if (!node) return -1;
+	if (node->prev) node->prev->next = node->next;
+	else *head = node->next;
+	if (node->next) node->next->prev = node->prev;
 	free(node);
-	return (1);
+	return 1;
 }
-/* fix for checker: (*head)->prev->next = (*head)->next; */
