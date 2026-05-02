b5bf502be510430932b2208d937181f7aa82ab3e
Shaden Alalwani <viuxwl7@gmail.com>
2026-05-02 12:47:22 +0300

    Add required double linked list files

---
+++ challenge/4-delete_dnodeint/add_dnodeint_end.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node, *tmp;
+	if (head == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
+	new_node->prev = NULL;
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+	tmp = *head;
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->next = new_node;
+	new_node->prev = tmp;
+	return (new_node);
+}
